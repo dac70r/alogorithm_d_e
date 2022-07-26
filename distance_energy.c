a69f2f80bb0431547195e07bdb571e6338efb889
Dennis Wong <denniswong178@gmail.com>
2022-07-26 15:58:33 +0800

    Clean

--- distance_energy.c
+++ distance_energy.c
@@ -16,33 +16,5 @@ void coefficient_array_init(uint8_t (*ptr)[]){
     }
 
 }
-/*
-void coefficient_array_init(uint8_t coefficient_array[]){
 
-    for(uint8_t x=0; x<DATA_POINTS_THIRTEEN; x++){
-        if(x==0 || x == DATA_POINTS_THIRTEEN-1){
-            //*(coefficient_array+x) = 1;
-            coefficient_array[x] = 1;
-        }
-        
-        else
-            {if(x%2==1){coefficient_array[x]=4;}else{coefficient_array[x]=2;}}
-        printf("%d\n",coefficient_array[x]); 
-    }
 
-}
-*/
-
-/*
-void coefficient_array_init(uint8_t* ptr){
-
-    *ptr = 233;
-
-}
-
-void coefficient_array_init_value(uint8_t ptr){
-
-    ptr = 123;
-    printf("from function: %d\n",ptr);
-}
-*/
